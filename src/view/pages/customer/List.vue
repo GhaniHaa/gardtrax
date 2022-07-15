9fc27bfaeaa65e9fb285fb97ec666ea278e624a4
rismayf <rismayf13@gmail.com>
2022-07-15 10:01:12 +0700

    change datatable in List customer page

--- src/view/pages/customer/List.vue
+++ src/view/pages/customer/List.vue
@@ -32,26 +32,76 @@
           <b-button variant="primary gutter-b" @click="handleButtonAdd()"
             >Tambah</b-button
           >
-          <b-form-group label-for="file-default">
-            <ve-table
-              :columns="columns"
-              :table-data="tableData"
-              :sort-option="sortOption"
-              :border-y="true"
-              :row-style-option="rowStyleOption"
-            />
-            <div class="table-pagination">
-              <ve-pagination
-                :total="totalCount"
-                :page-index="pageIndex"
-                :page-size="pageSize"
-                @on-page-number-change="pageNumberChange"
-                @on-page-size-change="pageSizeChange"
-                :border-y="true"
-                class="w-100"
-              />
+          <div class="row justify-content-between">
+            <div class="col-md-1">
+              <b-form-group class="mb-0">
+                <b-form-select
+                  id="per-page-select"
+                  v-model="perPage"
+                  :options="pageOptions"
+                  size="sm"
+                ></b-form-select>
+              </b-form-group>
             </div>
-          </b-form-group>
+            <div class="col-md-3">
+              <b-form-group class="mb-0">
+                <b-input-group>
+                  <template #prepend>
+                    <b-input-group-text>
+                      <i class="menu-icon flaticon-search"></i>
+                    </b-input-group-text>
+                  </template>
+                  <b-form-input
+                    v-model="search"
+                    placeholder="Search"
+                  ></b-form-input>
+                </b-input-group>
+              </b-form-group>
+            </div>
+          </div>
+          <b-table
+            hover
+            :items="tableData"
+            :fields="columns"
+            :per-page="perPage"
+            :current-page="currentPage"
+          >
+            <template #cell(action)="row">
+              <span class="mr-2" @click="handleEdit(row.item)">
+                <b-button
+                  variant="success"
+                  class="py-1 px-2"
+                  id="detail"
+                  title="detail"
+                >
+                  <i class="menu-icon flaticon-edit pr-0"></i>
+                </b-button>
+              </span>
+              <span @click="handleDelete(row.item)">
+                <b-button
+                  variant="danger"
+                  class="py-1 px-2"
+                  id="detail"
+                  title="detail"
+                >
+                  <i class="menu-icon flaticon-delete pr-0"></i>
+                </b-button>
+              </span>
+            </template>
+          </b-table>
+          <div class="row justify-content-end">
+            <div class="col-md-3">
+              <b-pagination
+                v-model="currentPage"
+                :total-rows="totalRows"
+                :per-page="perPage"
+                size="md"
+                align="fill"
+                class="ml-auto"
+                style="padding: 0"
+              ></b-pagination>
+            </div>
+          </div>
         </b-card>
       </div>
     </div>
@@ -88,7 +138,12 @@ export default {
       },
       search: "",
       pageIndex: 1,
+      //
+      totalRows: 10,
+      currentPage: 1,
       pageSize: 10,
+      perPage: 10,
+      pageOptions: [10, 15, { value: 100, text: "Show a lot" }],
       sortOption: {
         sortChange: (params) => {
           this.sortChange(params);
@@ -96,46 +151,40 @@ export default {
       },
       columns: [
         {
-          field: "id",
-          key: "a",
-          title: "ID",
+          key: "id",
+          label: "ID",
           align: "left",
           sortBy: "",
         },
         {
-          field: "parent",
-          key: "b",
-          title: "Parent",
+          key: "parent",
+          label: "Parent",
           align: "left",
           sortBy: "",
         },
         {
-          field: "nama",
-          key: "c",
-          title: "Nama",
+          key: "nama",
+          label: "Nama",
           align: "left",
           sortBy: "",
           width: "30%",
         },
         {
-          field: "cabang",
-          key: "d",
-          title: "Cabang",
+          key: "cabang",
+          label: "Cabang",
           align: "left",
           sortBy: "",
         },
         {
-          field: "tanggalPerjanjian",
-          key: "e",
-          title: "Tanggal Berlaku Perjanjian",
+          key: "tanggalPerjanjian",
+          label: "Tanggal Berlaku Perjanjian",
           align: "left",
           width: "",
           sortBy: "",
         },
         {
-          field: "",
-          key: "f",
-          title: "Aksi",
+          key: "",
+          label: "Aksi",
           align: "center",
           // eslint-disable-next-line
             renderBodyCell: ({ row, column, rowIndex }, h) => {
@@ -179,14 +228,14 @@ export default {
   components: {},
   computed: {
     tableData() {
-      const { pageIndex, pageSize } = this;
-      return DB_DATA.slice((pageIndex - 1) * pageSize, pageIndex * pageSize);
+      return DB_DATA
     },
     totalCount() {
       return DB_DATA.length;
     },
   },
   mounted() {
+    this.totalRows = this.tableData.length;
     this.$store.dispatch(SET_BREADCRUMB, [
       { title: "Dashboard", route: "/dashboard" },
       { title: "Customer" },
@@ -221,7 +270,7 @@ export default {
       DB_DATA = [];
       for (let i = 0; i < 1000; i++) {
         DB_DATA.push({
-          id: i,
+          id: i+1,
           parent: "Papah",
           nama: "Feri (Danru)",
           cabang: "BNI",
