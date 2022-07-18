5297cdf099d264f0e037ffaae28e87b7398f4d2e
rismayf <rismayf13@gmail.com>
2022-07-18 16:24:42 +0700

    change datatable in qrcode

--- src/view/pages/customer/QRCode/List.vue
+++ src/view/pages/customer/QRCode/List.vue
@@ -6,26 +6,76 @@
     <div class="row">
       <div class="col-xxl-12 col-12 col-md-12">
         <b-card>
-          <b-form-group label-for="file-default">
-            <ve-table
-              :columns="columns"
-              :table-data="tableData"
-              :sort-option="sortOption"
-              :border-y="true"
-              :row-style-option="rowStyleOption"
-            />
-            <div class="table-pagination">
-              <ve-pagination
-                :total="totalCount"
-                :page-index="pageIndex"
-                :page-size="pageSize"
-                @on-page-number-change="pageNumberChange"
-                @on-page-size-change="pageSizeChange"
-                :border-y="true"
-                class="w-100"
-              />
+          <div class="row justify-content-between">
+            <div class="col-md-1">
+              <b-form-group class="mb-0">
+                <b-form-select
+                  id="per-page-select"
+                  v-model="perPage"
+                  :options="pageOptions"
+                  size="sm"
+                ></b-form-select>
+              </b-form-group>
             </div>
-          </b-form-group>
+            <div class="col-md-3">
+              <b-form-group class="mb-0">
+                <b-input-group>
+                  <template #prepend>
+                    <b-input-group-text>
+                      <i class="menu-icon flaticon-search"></i>
+                    </b-input-group-text>
+                  </template>
+                  <b-form-input
+                    v-model="search"
+                    placeholder="Search"
+                  ></b-form-input>
+                </b-input-group>
+              </b-form-group>
+            </div>
+          </div>
+          <b-table
+            hover
+            :items="tableData"
+            :fields="columns"
+            :per-page="perPage"
+            :current-page="currentPage"
+          >
+            <template #cell(action)="row">
+              <span class="mr-2" @click="handleView(row.item)">
+                <b-button
+                  variant="primary"
+                  class="py-1 px-2"
+                  id="detail"
+                  title="detail"
+                >
+                  <i class="menu-icon flaticon-eye pr-0"></i>
+                </b-button>
+              </span>
+              <span class="mr-2" @click="handleEdit(row.item)">
+                <b-button
+                  variant="success"
+                  class="py-1 px-2"
+                  id="edit"
+                  title="edit"
+                >
+                  <i class="menu-icon flaticon-edit pr-0"></i>
+                </b-button>
+              </span>
+            </template>
+          </b-table>
+          <div class="row justify-content-end">
+            <div class="col-md-3">
+              <b-pagination
+                v-model="currentPage"
+                :total-rows="totalRows"
+                :per-page="perPage"
+                size="md"
+                align="fill"
+                class="ml-auto"
+                style="padding: 0"
+              ></b-pagination>
+            </div>
+          </div>
         </b-card>
       </div>
     </div>
@@ -62,7 +112,11 @@ export default {
       },
       search: "",
       pageIndex: 1,
+      totalRows: 10,
+      currentPage: 1,
       pageSize: 10,
+      perPage: 10,
+      pageOptions: [10, 15, { value: 100, text: "Show a lot" }],
       sortOption: {
         sortChange: (params) => {
           this.sortChange(params);
@@ -70,67 +124,28 @@ export default {
       },
       columns: [
         {
-          field: "nama",
-          key: "a",
-          title: "Nama",
+          key: "nama",
+          label: "Nama",
           align: "left",
           sortBy: "",
         },
         {
-          field: "kontak",
-          key: "b",
-          title: "No Kontak",
+          key: "kontak",
+          label: "No Kontak",
           align: "left",
           sortBy: "",
         },
         {
-          field: "phone",
-          key: "c",
-          title: "No Telpon",
+          key: "phone",
+          label: "No Telpon",
           align: "left",
           sortBy: "",
         },
         {
-          field: "",
-          key: "d",
-          title: "Aksi",
+          key: "action",
+          label: "Aksi",
           align: "center",
           width: "10%",
-          // eslint-disable-next-line
-            renderBodyCell: ({ row, column, rowIndex }, h) => {
-            return (
-              <span>
-                <b-button
-                  variant="success"
-                  class="py-1 px-2 mr-1"
-                  id="detail"
-                  title="edit"
-                  on-click={() => {
-                    this.handleEdit(row);
-                  }}
-                >
-                  <i
-                    class="menu-icon flaticon-edit pr-0"
-                    style="font-size: 13px"
-                  ></i>
-                </b-button>
-                <b-button
-                  variant="primary"
-                  class="py-1 px-2 mr-1"
-                  id="detail"
-                  title="view"
-                  on-click={() => {
-                    this.handleView(row);
-                  }}
-                >
-                  <i
-                    class="menu-icon flaticon-eye pr-0"
-                    style="font-size: 13px"
-                  ></i>
-                </b-button>
-              </span>
-            );
-          },
         },
       ],
     };
@@ -138,14 +153,14 @@ export default {
   components: {},
   computed: {
     tableData() {
-      const { pageIndex, pageSize } = this;
-      return DB_DATA.slice((pageIndex - 1) * pageSize, pageIndex * pageSize);
+      return DB_DATA;
     },
     totalCount() {
       return DB_DATA.length;
     },
   },
   mounted() {
+    this.totalRows = this.tableData.length;
     this.$store.dispatch(SET_BREADCRUMB, [
       { title: "Dashboard", route: "/dashboard" },
       { title: "Generate QR Code" },
